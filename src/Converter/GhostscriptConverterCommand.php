7772b41586a53b2b2348864d82fc3abd188733a5
reloxx13 <reloxx@interia.pl>
2021-03-02 11:20:53 +0100

    Update GhostscriptConverterCommand.php

--- src/Converter/GhostscriptConverterCommand.php
+++ src/Converter/GhostscriptConverterCommand.php
@@ -8,35 +8,28 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Xthiago\PDFVersionConverter\Converter;
 
-use Symfony\Component\Process\Process;
+namespace Xthiago\PDFVersionConverter\Converter;
 
 /**
  * Encapsulates the knowledge about gs command.
  *
  * @author Thiago Rodrigues <xthiago@gmail.com>
  */
-class GhostscriptConverterCommand
-{
-    /**
-     * @var Filesystem
-     */
-    protected $baseCommand = 'gs -sDEVICE=pdfwrite -dCompatibilityLevel=%s -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -dColorConversionStrategy=/LeaveColorUnchanged -dEncodeColorImages=false -dEncodeGrayImages=false -dEncodeMonoImages=false -dDownsampleMonoImages=false -dDownsampleGrayImages=false -dDownsampleColorImages=false -dAutoFilterColorImages=false -dAutoFilterGrayImages=false -dColorImageFilter=/FlateEncode -dGrayImageFilter=/FlateEncode  -sOutputFile=%s %s';
-
-    public function __construct()
-    {
-    }
-
-    public function run($originalFile, $newFile, $newVersion)
-    {
-        $command = sprintf($this->baseCommand, $newVersion, $newFile, escapeshellarg($originalFile));
-
-        $process = new Process([$command]);
-        $process->run();
-
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException($process->getErrorOutput());
-        }
-    }
+class GhostscriptConverterCommand {
+	/**
+	 * @var Filesystem
+	 */
+	protected $baseCommand = 'gs -sDEVICE=pdfwrite -dCompatibilityLevel=%s -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -dColorConversionStrategy=/LeaveColorUnchanged -dEncodeColorImages=false -dEncodeGrayImages=false -dEncodeMonoImages=false -dDownsampleMonoImages=false -dDownsampleGrayImages=false -dDownsampleColorImages=false -dAutoFilterColorImages=false -dAutoFilterGrayImages=false -dColorImageFilter=/FlateEncode -dGrayImageFilter=/FlateEncode  -sOutputFile=%s %s';
+	
+	public function __construct() {
+	}
+	
+	public function run($originalFile, $newFile, $newVersion) {
+		$command = sprintf($this->baseCommand, $newVersion, $newFile, escapeshellarg($originalFile));
+		@system($command, $result);
+		if ($result !== 0) {
+			throw new \RuntimeException($result);
+		}
+	}
 }
